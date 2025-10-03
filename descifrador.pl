4d37e3cf4afb8ba38805e8dbb39b93df9d550516
Saul-PC <saulpacheco407@gmail.com>
2025-10-02 19:24:51 -0600

    Funciones de entropía, no funciona el feedbackTotal

--- descifrador.pl
+++ descifrador.pl
@@ -1,4 +1,11 @@
 
+min(A, B, A):- A =< B,!.
+min(A, B, B):- A >= B,!.
+
+max(A, B, A):- A >= B, !.
+max(A, B, B):- A =< B, !.
+
+logaritmo(X, Res):- Res is log(X)/log(2).
 
 
 generarEspacioMuestral(_, 0, []):-!.
@@ -18,12 +25,45 @@ coincidencias([H|T], H, Cantidad):- coincidencias(T, H, CantidadNueva), !, Canti
 coincidencias([_H|T], X, Cantidad):- coincidencias(T, X, Cantidad), !.
 
 
-min(A, B, A):- A =< B,!.
-min(A, B, B):- A >= B,!.
+
 
 minimoIncognitas([], _, 0):-!.
-minimoIncognitas([GuessHead|GuessTail], ElementoUniverso, Minimo):- minimoIncognitas(GuessTail, ElementoUniverso, NuevoMinimo),
+minimoIncognitas([GuessHead|GuessTail], ElementoUniverso, Minimo):- eliminarRepeticiones(GuessHead, GuessTail, GuessRest),
+                                                                    minimoIncognitas(GuessRest, ElementoUniverso, NuevoMinimo),
                                                                     coincidencias([GuessHead|GuessTail], GuessHead, A),
                                                                     coincidencias(ElementoUniverso, GuessHead, B), min(A,B, MinimoParcial), Minimo is NuevoMinimo + MinimoParcial. 
 
-%hace falta el quitar en el que se mueve.
+eliminarRepeticiones(_E, [], []):-!.
+eliminarRepeticiones(E,[E|T],R):- eliminarRepeticiones(E, T, R),!.
+eliminarRepeticiones(E, [H|T],[H|R]):- eliminarRepeticiones(E, T, R),!.
+
+feedback(Guess, ElementoUniverso, [Aciertos, Incognitas]):- minimoIncognitas(Guess, ElementoUniverso, Minimo), 
+                                                 cantidadAciertos(Guess, ElementoUniverso, Aciertos), 
+                                                 Incognitas is Minimo - Aciertos, !.
+
+
+contarRepetidos(Feedback, Res):- sort(Feedback, Unicos), findall([Par, Cantidad],
+                                 (member(Par, Unicos), include(=(Par), Feedback, Filtrado),
+                                 length(Filtrado, Cantidad)), Res).
+
+
+calcularEntropiaAux([], _ , 0):-!.
+calcularEntropiaAux([[_, Cantidad]|T], CardinalidadGuesses, Entropia):- calcularEntropiaAux(T, CardinalidadGuesses, EntropiaNueva), 
+                                                                       ProbabilidadFeedback is Cantidad / CardinalidadGuesses,
+                                                                       logaritmo(ProbabilidadFeedback, Log),
+                                                                       Argumento is ProbabilidadFeedback * Log,
+                                                                       Entropia is EntropiaNueva + Argumento.
+
+calcularEntropia(Frecuencias, CardinalidadGuesses, Entropia):- calcularEntropiaAux(Frecuencias, CardinalidadGuesses, EntropiaParcial), Entropia is EntropiaParcial * (-1).
+
+feedbackTotal(ElementoUniverso, Guesses, FeedbackTotal):- findall(Feedback, (member(Guess, Guesses), feedback(Guess, ElementoUniverso, Feedback)), FeedbackTotal).
+
+
+entropiasAux([], _, _, [0, []]):-!.
+entropiasAux([ElementoUniverso|R], Guesses, CardinalidadGuesses,[EntropiaMaxima, ElementoMaximo]):- entropiasAux(R, Guesses, CardinalidadGuesses, [EntropiaActual, ElementoActual]), feedbackTotal(ElementoUniverso, Guesses, FeedbackTotal),
+                                                                             contarRepetidos(FeedbackTotal, Frecuencias),
+                                                                             calcularEntropia(Frecuencias, CardinalidadGuesses, Entropia),
+(Entropia >= EntropiaActual -> EntropiaMaxima is Entropia, ElementoMaximo = ElementoUniverso ; EntropiaMaxima is EntropiaActual, ElementoMaximo = ElementoActual).
+
+
+
